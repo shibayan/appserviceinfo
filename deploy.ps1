45d6b623c1c82cad0e55dddcda4596f463f6d419
shibayan <shiba-yan@outlook.com>
2016-07-25 16:15:33 +0900

    posh deployscript を修正

--- deploy.ps1
+++ deploy.ps1
@@ -99,28 +99,4 @@ exitWithMessageOnError "dotnet publish failed"
 exitWithMessageOnError "Kudu Sync failed"
 
 ##################################################################################################################################
-goto end
-
-:: Execute command routine that will echo out when error
-:ExecuteCmd
-setlocal
-set _CMD_=%*
-call %_CMD_%
-if "%ERRORLEVEL%" NEQ "0" echo Failed exitCode=%ERRORLEVEL%, command=%_CMD_%
-exit /b %ERRORLEVEL%
-
-:error
-endlocal
-echo An error has occurred during web site deployment.
-call :exitSetErrorLevel
-call :exitFromFunction 2>nul
-
-:exitSetErrorLevel
-exit /b 1
-
-:exitFromFunction
-()
-
-:end
-endlocal
-echo Finished successfully.
+echo "Finished successfully."
