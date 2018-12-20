14db47c05e4c21aaa496622e1ac68641ad4ba485
shibayan <me@shibayan.jp>
2018-12-20 16:44:00 +0900

    Update compatibility version

--- src/AppServiceInfo/Startup.cs
+++ src/AppServiceInfo/Startup.cs
@@ -21,7 +21,7 @@ namespace AppServiceInfo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc()
-                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddSwaggerGen(c =>
             {
