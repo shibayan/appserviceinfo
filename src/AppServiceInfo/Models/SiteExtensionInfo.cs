772a21a28a348448ae707753a574bd53fbccf3fb
shibayan <me@shibayan.jp>
2019-09-13 01:29:38 +0900

    Upgrade to ASP.NET Core 3.0

--- src/AppServiceInfo/Models/SiteExtensionInfo.cs
+++ src/AppServiceInfo/Models/SiteExtensionInfo.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections.Generic;
 
-using Newtonsoft.Json;
-
 namespace AppServiceInfo.Models
 {
     public class SiteExtensionInfo
     {
-        [JsonProperty("name")]
         public string Name { get; set; }
 
-        [JsonProperty("enabled")]
         public bool Enabled { get; set; }
 
-        [JsonProperty("versions")]
         public IList<VersionInfo> Versions { get; set; }
     }
 }
