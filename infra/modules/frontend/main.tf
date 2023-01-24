9cbfffb83befbc9f0fe5b191ed4e03ce2995a943
Tatsuro Shibamura <me@shibayan.jp>
2023-01-25 02:19:19 +0900

    Fixed boolean error

--- infra/modules/frontend/main.tf
+++ infra/modules/frontend/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_dns_a_record" "default" {
 }
 
 resource "azurerm_dns_txt_record" "default" {
-  count               = azurerm_static_site_custom_domain.default.validation_token ? 1 : 0
+  count               = azurerm_static_site_custom_domain.default.validation_token != "" ? 1 : 0
   name                = "@"
   zone_name           = var.dns_zone_name
   resource_group_name = var.resource_group_name
