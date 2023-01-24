3adad26b366f20eb94faf9cb1a6686c15634900b
Tatsuro Shibamura <me@shibayan.jp>
2023-01-24 14:46:07 +0900

    Fixed DNS TXT record name

--- infra/modules/frontend/main.tf
+++ infra/modules/frontend/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_dns_a_record" "default" {
 }
 
 resource "azurerm_dns_txt_record" "default" {
-  name                = "_dnsauth"
+  name                = "@"
   zone_name           = var.dns_zone_name
   resource_group_name = var.resource_group_name
   ttl                 = 300
