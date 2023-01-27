ae696a5d457bb040bb98abf4dccd1608ac01e8d6
Tatsuro Shibamura <me@shibayan.jp>
2023-01-27 13:16:16 +0900

    Adding multi-locations support (#70)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,34 +5,47 @@ import Platform from './components/Platform.vue'
 import Runtime from './components/Runtime.vue'
 import SiteExtension from './components/SiteExtension.vue'
 
+const locations = ["japaneast", "westus2", "eastus2"]
 const currentLocation = ref<string>("japaneast")
 </script>
 
 <template>
-  <Suspense>
+  <Suspense timeout="0">
     <template #default>
-      <Platform :location="currentLocation" />
+      <Platform :location="currentLocation" :key="currentLocation" />
     </template>
     <template #fallback>
       <span>Loading...</span>
     </template>
   </Suspense>
   <hr />
-  <Suspense>
+  <Suspense timeout="0">
     <template #default>
-      <Runtime :location="currentLocation" />
+      <Runtime :location="currentLocation" :key="currentLocation" />
     </template>
     <template #fallback>
       <span>Loading...</span>
     </template>
   </Suspense>
   <hr />
-  <Suspense>
+  <Suspense timeout="0">
     <template #default>
-      <SiteExtension :location="currentLocation" />
+      <SiteExtension :location="currentLocation" :key="currentLocation" />
     </template>
     <template #fallback>
       <span>Loading...</span>
     </template>
   </Suspense>
+  <hr />
+  <div class="field-body">
+    <div class="field">
+      <div class="control">
+        <div class="select">
+          <select v-model="currentLocation">
+            <option v-for="location in locations" :value="location">{{ location }}</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
