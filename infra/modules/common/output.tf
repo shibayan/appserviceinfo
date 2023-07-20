d88fb44ae25bf533332f410ea61cef85f30730e2
Tatsuro Shibamura <me@shibayan.jp>
2023-07-20 21:34:50 +0900

    Update terraform provider for azurerm

--- infra/modules/common/output.tf
+++ infra/modules/common/output.tf
@@ -5,3 +5,7 @@ output "resource_group_name" {
 output "dns_zone_name" {
   value = azurerm_dns_zone.default.name
 }
+
+output "storage_account_name" {
+  value = azurerm_storage_account.default.name
+}
