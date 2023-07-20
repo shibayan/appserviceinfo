2165f0edad1c89f82d7f897ed458be802e892d7f
Tatsuro Shibamura <me@shibayan.jp>
2023-07-20 21:33:32 +0900

    Add storage account

--- infra/modules/common/main.tf
+++ infra/modules/common/main.tf
@@ -7,3 +7,26 @@ resource "azurerm_dns_zone" "default" {
   name                = "appservice.info"
   resource_group_name = azurerm_resource_group.default.name
 }
+
+resource "azurerm_storage_account" "default" {
+  name                     = "stgraffias"
+  location                 = azurerm_resource_group.default.location
+  resource_group_name      = azurerm_resource_group.default.name
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  account_replication_type = "LRS"
+
+  blob_properties {
+    cors_rule {
+      allowed_methods    = ["GET"]
+      allowed_origins    = ["https://appservice.info"]
+      max_age_in_seconds = 600
+    }
+  }
+}
+
+resource "azurerm_storage_container" "default" {
+  name                  = "metadata"
+  storage_account_name  = azurerm_storage_account.default.name
+  container_access_type = "blob"
+}
