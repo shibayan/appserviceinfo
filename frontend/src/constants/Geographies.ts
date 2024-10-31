e07091678992ab8551b7d2f51d0351f0a9f5182e
Tatsuro Shibamura <me@shibayan.jp>
2024-10-31 14:02:05 +0900

    Adding Israel Central (#258)

--- frontend/src/constants/Geographies.ts
+++ frontend/src/constants/Geographies.ts
@@ -2,7 +2,7 @@ export class Geographies {
   private static geographies = [
     { name: "Americas", locations: ["brazilsouth", "canadacentral", "centralus", "eastus", "eastus2", "southcentralus", "westus2", "westus3"] },
     { name: "Europe", locations: ["francecentral", "northeurope", "norwayeast", "switzerlandnorth", "uksouth", "westeurope"] },
-    { name: "Middle East", locations: ["qatarcentral", "uaenorth"] },
+    { name: "Middle East", locations: ["qatarcentral", "uaenorth", "israelcentral"] },
     { name: "Asia Pacific", locations: ["australiaeast", "centralindia", "eastasia", "japaneast", "koreacentral", "southeastasia"] }
   ]
 
