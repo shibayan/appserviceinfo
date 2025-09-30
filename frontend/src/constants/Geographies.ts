d923730b70c02c0d3ef031e7ded9feb9e4adb9e9
Tatsuro Shibamura <me@shibayan.jp>
2025-09-30 12:30:43 +0900

    Remove quota error location

--- frontend/src/constants/Geographies.ts
+++ frontend/src/constants/Geographies.ts
@@ -1,8 +1,8 @@
 export class Geographies {
   private static geographies = [
-    { name: "Americas", locations: ["brazilsouth", "canadacentral", "centralus", "eastus", "eastus2", "southcentralus", "westus2", "westus3"] },
+    { name: "Americas", locations: ["brazilsouth", "canadacentral", "centralus", "eastus", "eastus2", "westus3"] },
     { name: "Europe", locations: ["francecentral", "northeurope", "norwayeast", "switzerlandnorth", "uksouth", "westeurope"] },
-    { name: "Middle East", locations: ["qatarcentral", "uaenorth", "israelcentral"] },
+    { name: "Middle East", locations: ["uaenorth", "israelcentral"] },
     { name: "Asia Pacific", locations: ["australiaeast", "centralindia", "eastasia", "japaneast", "japanwest", "koreacentral", "southeastasia"] }
   ]
 
